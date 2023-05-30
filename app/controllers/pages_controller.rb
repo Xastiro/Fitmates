4eb8c822d5c6e8766d7c0042caa39060338a7b5c
matthias seeburger <seeburger.matthias@gmail.com>
2023-05-30 15:58:48 +0800

    Added map box to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :welcome ]
 
   def home
+    @workouts = Workout.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @workouts.geocoded.map do |workout|
+      {
+        lat: workout.latitude,
+        lng: workout.longitude
+      }
+    end
   end
 end
