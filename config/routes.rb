b1110c13e6d295205ed4e147af06cd04c155e265
matthias seeburger <seeburger.matthias@gmail.com>
2023-05-30 15:09:24 +0800

    Navbar draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#welcome"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   # root "rails/welcome#index"
+
+  get 'home', to: 'pages#home'
+
 end
