7476da3f14bd640e6f366c42f05027a8a1d42a4f
matthias seeburger <seeburger.matthias@gmail.com>
2023-06-01 15:54:21 +0800

    Added time picker in new workout form

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -4,9 +4,21 @@ import flatpickr from "flatpickr";
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   // Inform the controller that it has two targets in the form, which are our inputs
-  static targets = [ "date" ]
+  static targets = [ "date", "startTime", "endTime" ]
 
   connect() {
-    flatpickr(this.element, {});
+    flatpickr(this.dateTarget, {});
+    flatpickr(this.startTimeTarget, {
+                                      enableTime: true,
+                                      noCalendar: true,
+                                      dateFormat: "H:i",
+                                      time_24hr: true
+                                    });
+    flatpickr(this.endTimeTarget, {
+                                    enableTime: true,
+                                    noCalendar: true,
+                                    dateFormat: "H:i",
+                                    time_24hr: true
+                                  });
   }
 }
